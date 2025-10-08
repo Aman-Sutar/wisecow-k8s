56cf6c885dce9576ecadb33859c1e36377a9e51b
Aman <amansutar02@gmail.com>
2025-10-08 14:10:48 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -1,33 +1,46 @@
-# Use the official minimal Alpine Linux image
-FROM alpine:latest
+#!/usr/bin/env bash
 
-# Install dependencies for the app (bash, nc, fortune, cowsay, node.js, and npm)
-RUN apk update && apk add --no-cache \
-    bash \
-    ncurses \
-    fortune \
-    netcat-openbsd \
-    nodejs \
-    npm \
-    dos2unix  # Add dos2unix utility for handling Windows line endings
+SRVPORT=4499
+RSPFILE=response
 
-# Install cowsay via npm
-RUN npm install -g cowsay
+rm -f $RSPFILE
+mkfifo $RSPFILE
 
-# Set the working directory to /app
-WORKDIR /app
+get_api() {
+	read line
+	echo $line
+}
 
-# Copy the Wisecow script to the container
-COPY wisecow.sh /app/
+handleRequest() {
+    # 1) Process the request
+	get_api
+	mod=`fortune`
 
-# Ensure the script has Unix-style line endings (if necessary)
-RUN dos2unix wisecow.sh
+cat <<EOF > $RSPFILE
+HTTP/1.1 200
 
-# Make the script executable
-RUN chmod +x wisecow.sh
 
-# Expose the port the application will run on
-EXPOSE 4499
+<pre>`cowsay $mod`</pre>
+EOF
+}
 
-# Command to run the script when the container starts
-CMD ["bash", "./wisecow.sh"]
+prerequisites() {
+	command -v cowsay >/dev/null 2>&1 &&
+	command -v fortune >/dev/null 2>&1 || 
+		{ 
+			echo "Install prerequisites."
+			exit 1
+		}
+}
+
+main() {
+	prerequisites
+	echo "Wisdom served on port=$SRVPORT..."
+
+	while [ 1 ]; do
+		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
+		sleep 0.01
+	done
+}
+
+main
